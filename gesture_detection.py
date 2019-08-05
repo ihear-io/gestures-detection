6db2e21da0abd3b7b7d5bbe11e0568deac17b88c
mhashim6 <m6hashim@gmail.com>
2019-08-05 18:33:59 +0200

    refactored to handle multiple images/frames.

--- gesture_detection.py
+++ gesture_detection.py
@@ -5,12 +5,52 @@ import numpy as np
 from utils import import_open_pose
 import env_vars
 
-op = import_open_pose()
+_op = import_open_pose()
+_op_wrapper = _op.WrapperPython()
 
-params = {"model_folder": env_vars.MODEL_LOC, "model_pose": "COCO", "number_people_max": 1, "net_resolution": "-1x64"}
+params = {
+    "model_folder": env_vars.MODEL_LOC,
+    "model_pose": "COCO",
+    "number_people_max": 1,
+    "net_resolution": "-1x64"
+}
 
 
-def hand_keypoints(img, debug=False, err_thresh=0.1, margin=250.0, l=450.0):
+def _hands_rectangles(
+        images: List,
+        debug=False,
+        margin: float = 250.0,
+        l: float = 450.0) -> List[List[_op.Rectangle]]:
+    _op_wrapper.configure(params)
+    _op_wrapper.start()
+    hands = []
+    for img in images:
+        datum = _op.Datum()
+        datum.cvInputData = img
+        _op_wrapper.emplaceAndPop([datum])
+
+        if debug:
+            cv2.imshow("body pose", datum.cvOutputData)
+
+        x_left, y_left, _ = datum.poseKeypoints[0][7]  # left wrist
+        x_right, y_right, _ = datum.poseKeypoints[0][4]  # right wrist
+
+        hand_rectangles = [
+            [
+                _op.Rectangle(x_left - margin, y_left - margin, l, l),  # left hand
+                _op.Rectangle(x_right - margin, y_right - margin, l, l),  # right hand
+            ]
+        ]
+        hands.append(hand_rectangles)
+    return hands
+
+
+def hand_keypoints(
+        images: List,
+        debug=False,
+        err_thresh: float = 0.1,
+        margin: float = 250.0,
+        l: float = 450.0) -> List[Tuple[np.ndarray, np.ndarray]]:
     """
     Uses body pose estimation to estimate lwrist & rwrist positions,
     from which we use `openpose` to obtain hand keypoints vectors. 
@@ -23,27 +63,8 @@ def hand_keypoints(img, debug=False, err_thresh=0.1, margin=250.0, l=450.0):
     `margin`: margin of each hand rectangle.\n
     `l`: squared rectangle length.\n
     """
-    op_wrapper = op.WrapperPython()
-    op_wrapper.configure(params)
-    op_wrapper.start()
 
-    datum = op.Datum()
-    datum.cvInputData = img
-    op_wrapper.emplaceAndPop([datum])
-
-    if debug:
-        cv2.imshow("body pose", datum.cvOutputData)
-
-    x_left, y_left, _ = datum.poseKeypoints[0][7]  # left wrist
-    x_right, y_right, _ = datum.poseKeypoints[0][4]  # righr wrist
-
-    hand_rectangles = [
-        [
-            op.Rectangle(x_left - margin, y_left - margin, l, l),  # left hand
-            op.Rectangle(x_right - margin, y_right - margin, l, l),  # right hand
-        ]
-    ]
-
+    hands = _hands_rectangles(images, debug, margin, l)
     hand_params = {
         "model_folder": env_vars.MODEL_LOC,
         "model_pose": "COCO",
@@ -52,41 +73,42 @@ def hand_keypoints(img, debug=False, err_thresh=0.1, margin=250.0, l=450.0):
         "hand_detector": 2,
         "body": 0,
     }
-    op_wrapper.configure(hand_params)
-    op_wrapper.start()
-    datum = op.Datum()
-    datum.cvInputData = img
-    datum.handRectangles = hand_rectangles
-    op_wrapper.emplaceAndPop([datum])
+    _op_wrapper.configure(hand_params)
+    _op_wrapper.start()
+    keypoints: List[Tuple[np.ndarray, np.ndarray]] = []
+    for i, img in enumerate(images):
+        datum = _op.Datum()
+        datum.cvInputData = img
+        datum.handRectangles = hands[i]
+        _op_wrapper.emplaceAndPop([datum])
 
-    ls = datum.handKeypoints[0][0]  # left
-    rs = datum.handKeypoints[1][0]  # right
-    print(type(ls))
-    l_avg = sum(map(lambda y: y[2], ls)) / 21.0
-    print("left avg: ", l_avg)
-    r_avg = sum(map(lambda y: y[2], rs)) / 21.0
-    print("right avg: ", r_avg)
+        ls = datum.handKeypoints[0][0]  # left
+        rs = datum.handKeypoints[1][0]  # right
 
-    # sanitization
-    if l_avg < err_thresh:
-        ls *= (0.0, 0.0, 1.0)
-        # ls = np.array(list(map(lambda ns: np.array([0.0, 0.0, ns[2]]), ls)))
-    if r_avg < err_thresh:
-        rs *= (0.0, 0.0, 1.0)
-        # rs = np.array(list(map(lambda ns: np.array([0.0, 0.0, ns[2]]), rs)))
+        l_avg = sum(map(lambda y: y[2], ls)) / 21.0
+        print("left avg: ", l_avg)
+        r_avg = sum(map(lambda y: y[2], rs)) / 21.0
+        print("right avg: ", r_avg)
 
-    print("Left hand keypoints:\n", ls)
-    print("Right hand keypoints:\n", rs)
+        # sanitization
+        if l_avg < err_thresh:
+            ls *= (0.0, 0.0, 1.0)
+        if r_avg < err_thresh:
+            rs *= (0.0, 0.0, 1.0)
+        keypoints.extend((ls, rs))
 
-    if debug:
-        cv2.imshow("hand key points", datum.cvOutputData)
-        cv2.waitKey(0)
-        cv2.destroyAllWindows()
-    return ls, rs
+        if debug:
+            print("Left hand keypoints:\n", ls)
+            print("Right hand keypoints:\n", rs)
+            cv2.imshow("hand key points", datum.cvOutputData)
+            cv2.waitKey(0)
+            cv2.destroyAllWindows()
+
+    return keypoints
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("--image_path", help="Process an image. Read all standard formats (jpg, png, bmp, etc.).")
     args = parser.parse_known_args()
-    hand_keypoints(cv2.imread(args[0].image_path), True)
+    hand_keypoints([cv2.imread(args[0].image_path)], True)
