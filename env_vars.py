3c7d55d47fbd9907d19d0bdccb0cf6c460ba0817
mhashim6 <m6hashim@gmail.com>
2019-08-15 21:03:15 +0200

    refactoring, updated code style.

--- env_vars.py
+++ env_vars.py
@@ -3,9 +3,6 @@ import os
 OPEN_POSE_LOC_UBUNTU = "/usr/local/python"
 OPEN_POSE_LOC_WIN = "INSERT OPEN POSE PATH HERE"  # TODO
 
-if os.name == 'nt':  # windows
-    OPEN_POSE_LOC = OPEN_POSE_LOC_WIN
-else:
-    OPEN_POSE_LOC = OPEN_POSE_LOC_UBUNTU
+OPEN_POSE_LOC = OPEN_POSE_LOC_WIN if os.name == 'nt' else OPEN_POSE_LOC_UBUNTU
 
 MODEL_LOC = "/home/mhashim6/dev/openpose/models/"
